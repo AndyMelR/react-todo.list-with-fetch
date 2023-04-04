b1feb7444549bdd7dc61bb418f2f917ddbe07593
AndyMelR <ameleror@hotmail.es>
2023-04-04 17:48:07 +0000

    FIX: fetch put

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-  //const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
@@ -24,16 +24,42 @@ const Home = () => {
     fetchTodos();
   }, []);
 
-  useEffect(() => {
+ 
     let html = todos.map((todo) => `<li>${todo.label}</li>`).join("");
     html = `<ul>${html}</ul>`;
-  }, [todos]);
+
+
+  const handleSubmit = async (e) => {
+    const newTodo = { label: inputValue, done: false };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    try {
+      const url = "https://assets.breatheco.de/apis/fake/todos/user/ameleror";
+      const options = {
+        method: "PUT",
+        body: JSON.stringify(newTodos),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        console.log(`Hubo un error ${response.status} en el request`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    setInputValue("");
+  };
 
   return (
     <>
       <div className="container">
         <h1>todos</h1>
         <div className="container sombra" id="notebook">
+          <form onSubmit={handleSubmit}>
+            <input value={inputValue} placeholder=" Write your task" onChange={(e) => setInputValue(e.target.value)} />
+          </form>
           <ul>
             {todos.map((todo, index) => (
               <li key={index}>
@@ -53,6 +79,9 @@ const Home = () => {
             <li id="task-counter">
               <div>{todos.length} task left</div>
             </li>
+            <button type="reset" onClick={() => setTodos([])}>
+               Clear
+            </button>
           </ul>
         </div>
       </div>
